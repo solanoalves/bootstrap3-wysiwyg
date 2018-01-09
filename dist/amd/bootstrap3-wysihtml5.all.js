04382d83716117b7eaef28fc4225181432089884
Solano <solanoalves@gmail.com>
2018-01-09 17:01:44 -0200

    Modificacoes no parse paste

--- dist/amd/bootstrap3-wysihtml5.all.js
+++ dist/amd/bootstrap3-wysihtml5.all.js
@@ -15534,11 +15534,28 @@ function program33(depth0,data) {
           "target": "_blank"
          }
         },
+        "div": {
+            "one_of_type": {
+                "alignment_object": 1
+            },
+            "remove_action": "unwrap",
+            "keep_styles": {
+                "textAlign": 1,
+                "float": 1
+            },
+            "add_style": {
+                "align": "align_text"
+            },
+            "check_attributes": {
+                "id": "any",
+                "contenteditable": "any"
+            }
+        },
         "b": {},
         "br": {},
-        "div": {},
         "em": {},
         "i": {},
+        "u": {},
         "li": {},
         "ol": {},
         "p": {},
@@ -15638,6 +15655,45 @@ function program33(depth0,data) {
         '75': 'createLink'// K
       }
     };
+
+    (function(w5) {
+    // Paste cleanup rules universal for all rules (also applied to content copied from editor)
+    var commonRules = w5.lang.object(defaultOptions.parserRules).clone(true);
+    commonRules.comments    = false;
+    commonRules.selectors   = { "a u": "unwrap"};
+    commonRules.tags.style  = { "remove": 1 };
+    commonRules.tags.script = { "remove": 1 };
+    commonRules.tags.head = { "remove": 1 };
+
+    // Paste cleanup for unindentified source
+    var universalRules = w5.lang.object(commonRules).clone(true);
+    universalRules.tags.div.one_of_type.alignment_object = 1;
+    universalRules.tags.div.remove_action = "unwrap";
+    universalRules.tags.div.check_attributes.style = false;
+    universalRules.tags.div.keep_styles = {
+        "textAlign": /^((left)|(right)|(center)|(justify))$/i,
+        "float": 1
+    };
+    universalRules.tags.span.keep_styles = false;
+
+    // Paste cleanup for MS Office
+    // TODO: should be extended to stricter ruleset, as current set will probably not cover all Office bizarreness
+    var msOfficeRules = w5.lang.object(universalRules).clone(true);
+    msOfficeRules.classes = {};
+
+    defaultOptions.pasteParserRulesets = [
+        {
+            condition: /<font face="Times New Roman"|class="?Mso|style="[^"]*\bmso-|style='[^'']*\bmso-|w:WordDocument|class="OutlineElement|id="?docs\-internal\-guid\-/i,
+            set: msOfficeRules
+        },{
+            condition: /<meta name="copied-from" content="wysihtml5">/i,
+            set: commonRules
+        },{
+            set: universalRules
+        }
+    ];
+
+  })(wysihtml5);
 
     if (typeof $.fn.wysihtml5.defaultOptionsCache === 'undefined') {
       $.fn.wysihtml5.defaultOptionsCache = $.extend(true, {}, $.fn.wysihtml5.defaultOptions);
